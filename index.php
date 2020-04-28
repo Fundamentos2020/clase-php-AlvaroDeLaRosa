012fa2fff8608b4191da503068a38284ff8a75e6
AlvaroDeLaRosa <49072538+AlvaroDeLaRosa@users.noreply.github.com>
2020-04-28 18:37:27 -0500

    Upload 2(28_04)

--- index.php
+++ index.php
@@ -1,14 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Prueba PHP</h1>
-    <?php
-        echo "Hola mundo";
-    ?>
-</body>
-</html>
+<?php
+
+//Declaración de variables
+$a="Hola Mundo";
+$b=3;
+$c=2.5;
+
+//Funciones
+function miFuncion() {
+    global $a, $b;
+
+    $a="Cambiar texto";
+}
+
+miFuncion();
+//echo $a;
+
+//echo $a." ".$b." ".$c;
+
+//Arreglos
+$array_1=array();
+$array_2=array();
+
+$array_1[]=1;
+$array_1[]=2;
+$array_1[]=3;
+
+//var_dump($array_1);
+
+$array_2['Valor1']=1;
+$array_2['Valor2']=2;
+$array_2['Valor3']=3;
+
+//var_dump($array_2);
+//echo $array_2['Valor2'];
+
+$alumno['cve_unica']="282816";
+$alumno['nombre']="Alvaro";
+$alumno['apellidos']="De la Rosa";
+
+//$json=json_encode($alumno)
+
+$json='{"cve_unica":"282816","nombre":"Alvaro","apellidos":"De la Rosa"}';
+
+$alumno_aux=json_decode($json);
+
+//var_dump($alumno_aux);
+
+//echo $alumno_aux->cve_unica;
+
+class Aux {
+    public $x;
+    public $y;
+}
+
+$aux=new Aux();
+
+$aux->x="Valor X";
+
+var_dump($aux);
+?>
