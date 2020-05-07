a5b799d0d8b16448c4570b54d24ec436f5e98da3
AlvaroDeLaRosa <49072538+AlvaroDeLaRosa@users.noreply.github.com>
2020-05-07 18:59:17 -0500

    Upload (7/5)

---
+++ conexion_db.php
@@ -0,0 +1,43 @@
+<?php
+
+class Tarea {
+    public $_id;
+    public $_titulo;
+    public $_descripcion;
+    public $_fecha_limite;
+    public $_completada;
+    public $_categoria_id;
+
+    public function __construct($id, $titulo, $descripcion, $fecha_limite, $completada, $categoria_id) {
+        $this->_id=$id;
+        $this->_titulo=$titulo;
+        $this->_descripcion=$descripcion;
+        $this->_fecha_limite=$fecha_limite;
+        $this->_completada=$completada;
+        $this->_categoria_id=$categoria_id;
+    }
+}
+
+try {
+    $dns='mysql:host=localhost;dbname=lista_tareas';
+    $username='root';
+    $password='';
+    $connection=new PDO($dns, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $completada='SI';
+    $query=$connection->prepare('SELECT * FROM tareas WHERE completada=:completada');
+    $query->bindParam(':completada', $completada, PDO::PARAM_STR);
+    $query->execute();
+    $tareas=array();
+    while($row=$query->fetch(PDO::FETCH_ASSOC)) {
+        $tarea=new Tarea($row['id'], $row['titulo'], $row['descripcion'], $row['fecha_limite'], $row['completada'], $row['categoria_id']);
+        $tareas[]=$tarea;
+    }
+    var_dump($tareas);
+}
+catch(PDOException $e) {
+    echo 'Error'.$e;
+}
+
+?>
